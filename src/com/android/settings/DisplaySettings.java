81913dbee336eb5eba22934a97d4ffaf9a834359
Anushek Prasal <anushekprasal@gmail.com>
2018-11-14 14:40:04 +0530

    Add QS header styles [2/3]

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -37,6 +37,7 @@ import com.android.settings.display.FontPickerPreferenceController;
 import com.android.settings.display.NightDisplayPreferenceController;
 import com.android.settings.display.NightModePreferenceController;
 import com.android.settings.display.QsTileStylePreferenceController;
+import com.android.settings.display.QsHeaderStylePreferenceController;
 import com.android.settings.display.ScreenSaverPreferenceController;
 import com.android.settings.display.ShowOperatorNamePreferenceController;
 import com.android.settings.display.TapToWakePreferenceController;
@@ -140,6 +141,7 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new TimeoutPreferenceController(context, KEY_SCREEN_TIMEOUT));
         controllers.add(new VrDisplayPreferenceController(context));
         controllers.add(new QsTileStylePreferenceController(context));
+        controllers.add(new QsHeaderStylePreferenceController(context));
         controllers.add(new ShowOperatorNamePreferenceController(context));
         controllers.add(new WallpaperPreferenceController(context));
         controllers.add(new ThemePreferenceController(context));

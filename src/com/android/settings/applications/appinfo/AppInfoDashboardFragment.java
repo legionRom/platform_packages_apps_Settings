f21a62d1b8631a2ee556385b04173325f83fd406
mdeejay <mdjrussia@gmail.com>
2019-02-08 01:19:33 +0100

    Revert "Implement per-app thermal control [1/3]"
    This reverts commit fbb62a571c7331ce8e69c78b7b93bf1c5e094629.

--- src/com/android/settings/applications/appinfo/AppInfoDashboardFragment.java
+++ src/com/android/settings/applications/appinfo/AppInfoDashboardFragment.java
@@ -159,7 +159,6 @@ public class AppInfoDashboardFragment extends DashboardFragment
         super.onAttach(context);
         final String packageName = getPackageName();
         use(TimeSpentInAppPreferenceController.class).setPackageName(packageName);
-        use(ThermalControlPerAppPreferenceController.class).setPackageName(packageName);
 
         use(AppDataUsagePreferenceController.class).setParentFragment(this);
         final AppInstallerInfoPreferenceController installer =

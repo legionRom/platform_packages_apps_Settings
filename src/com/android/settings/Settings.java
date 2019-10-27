d46c4d6583bd4e0f031000970a4d71d82b59e922
Rajkale99 <kaleraj.rk@gmail.com>
2019-10-27 11:49:36 +0000

    Inject Legion Settings

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -177,4 +177,6 @@ public class Settings extends SettingsActivity {
     public static class AccountDashboardActivity extends SettingsActivity {}
     public static class SystemDashboardActivity extends SettingsActivity {}
 
+    // LegionSettings
+    public static class LegionSettingsActivity extends SettingsActivity {}
 }
